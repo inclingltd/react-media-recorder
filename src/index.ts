8ef7014bbc3994e7cb0444d18a70cd2b34835e1c
Chris Calitz <info@chriscalitz.com>
2025-08-27 14:39:37 +0100

    Test another fix

--- src/index.ts
+++ src/index.ts
@@ -215,8 +215,8 @@ export function useReactMediaRecorder({
   };
 
   const onRecordingActive = ({ data }: BlobEvent) => {
-    let chunks;
-    chunks = chunks ? chunks.concat(data) : [];
+    let [chunks] = mediaChunks.current;
+    chunks = chunks.concat(data);
     mediaChunks.current.push(URL.createObjectURL(new Blob(chunks, { type: chunks[0]?.type })));
   };
 
